c11adf2190dc7df1457104d695dd9ac76c042c21
James Lee <jamesleex86@gmail.com>
2022-08-26 10:19:29 -0700

    added handleValidationErrors middleware

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -8,18 +8,14 @@ const router = express.Router();
 
 
 // Restore session user
-router.get(
-    '/',
-    restoreUser,
-    (req, res) => {
-      const { user } = req;
-      if (user) {
-        return res.json({
-          user: user.toSafeObject()
-        });
-      } else return res.json({});
-    }
-  );
+router.get('/', restoreUser, (req, res) => {
+    const { user } = req;
+    if (user) {
+    return res.json({
+        user: user.toSafeObject()
+    });
+    } else return res.json({});
+});
 
   
   
