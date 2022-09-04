2e862d1df19242f8f398487b2ff89a54ef0aeabd
James Lee <jamesleex86@gmail.com>
2022-09-04 11:07:21 -0700

    Testing ROUND fn in heroku

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -50,7 +50,9 @@ router.get('/', async (req, res) => {
             where: {
                 spotId: spot.id
             },
-            attributes: [[sequelize.fn('ROUND', sequelize.fn('AVG', sequelize.col('stars'))), 'avgRating']],
+            // attributes: [[sequelize.fn('ROUND', sequelize.fn('AVG', sequelize.col('stars'))), 'avgRating']],
+            attributes: [[sequelize.fn('AVG', sequelize.col('stars')), 'avgRating']],
+
             raw: true
         });
 
