bf6320d8ab42a545df8b2c8a74d5514a6ede99ec
James Lee <jamesleex86@gmail.com>
2022-09-21 07:22:30 -0700

    Completed form for AddSpotForm.js

--- frontend/src/components/AddSpotFormModal/AddSpotForm.js
+++ frontend/src/components/AddSpotFormModal/AddSpotForm.js
@@ -8,11 +8,17 @@ function AddSpotForm() {
     const dispatch = useDispatch();
 
     const [address, setAddress] = useState("");
+    const [city, setCity] = useState("");
+    const [state, setState] = useState("");
+    const [country, setCountry] = useState("");
+    const [lat, setLat] = useState("");
+    const [lng, setLng] = useState("");
+    const [name, setName] = useState("");
+    const [description, setDescription] = useState("");
     const [price, setPrice] = useState("");
     
     const submitHandler = (e) => {
         e.preventDefault();
-
         const newSpotDetails = {
             address,
             city,
@@ -24,31 +30,46 @@ function AddSpotForm() {
             description,
             price,
         }
-
         dispatch(createSpot(newSpotDetails))
     }
 
-
-
-    
     return (
         <form onSubmit={submitHandler}>
             <input 
             onChange={e => setAddress(e.target.value)}
             value={address}/>
             <input 
-            type="number"
-            onChange={e  => setPrice(e.target.value)}
-            value={price}
-            step="0.01"
-            />
+            onChange={e => setCity(e.target.value)}
+            value={city}/>
+            <input
+            onChange={e => setState(e.target.value)}
+            value={state}/>
+            <input
+            onChange={e => setCountry(e.target.value)}
+            value={country}/>
             <input 
             type="number"
             onChange={e => setLat(e.target.value)}
             value={lat}
             step="0.00001"
             />
-        
+            <input
+            type="number"
+            onChange={e => setLng(e.target.value)}
+            value={lng}
+            />
+            <input
+            onChange={e => setName(e.target.value)}
+            value={name}/>
+            <input
+            onChange={e => setDescription(e.target.value)}
+            value={description}/>
+            <input 
+            type="number"
+            onChange={e  => setPrice(e.target.value)}
+            value={price}
+            step="0.01"
+            />
         </form>
     )
 }
