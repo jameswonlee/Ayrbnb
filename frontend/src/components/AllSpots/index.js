d8c16f706dc3787f3e5bf50eeb3ebc39122a08c5
James Lee <jamesleex86@gmail.com>
2022-09-22 10:21:03 -0700

    minor changes

--- frontend/src/components/AllSpots/index.js
+++ frontend/src/components/AllSpots/index.js
@@ -3,6 +3,7 @@ import { useEffect } from 'react';
 import { getAllSpots } from '../../store/spots';
 import SpotCard from '../SpotCard';
 import AddSpotFormModal from '../AddSpotFormModal/AddSpotFormModal';
+import './AllSpots.css'
 
 
 function DisplayAllSpots() {
@@ -10,21 +11,22 @@ function DisplayAllSpots() {
 
     const allSpots = useSelector(state => Object.values(state.spots.spots));
     const sessionUser = useSelector(state => state.session.user);
-    // console.log('DisplayAllSpots --> allSpots', allSpots)
+    console.log('DisplayAllSpots --> allSpots', allSpots)
 
     useEffect(() => {
         dispatch(getAllSpots())
     }, [])
 
-
     return (
-        <div className="outer">
+        <div className="content-wrapper">
             <h1>Spots List</h1>
-            {sessionUser && <AddSpotFormModal />}
-            <div className='spot-card'>
-                {allSpots.map(spot => (
-                    <SpotCard key={spot.id} spot={spot} />
-                ))}
+            <div className="all-spots">
+                {sessionUser && <AddSpotFormModal />}
+                <div className='spot-cards'>
+                    {allSpots.map(spot => (
+                        <SpotCard key={spot.id} spot={spot} />
+                    ))}
+                </div>
             </div>
         </div>
     )
