056734c9f4cd6cbf3c8f34e5ed51c15acb945fd4
James Lee <jamesleex86@gmail.com>
2022-09-30 16:15:44 -0700

    Undo changes for test

--- backend/utils/validation.js
+++ backend/utils/validation.js
@@ -5,36 +5,36 @@ const { validationResult } = require('express-validator');
 const handleValidationErrors = (req, _res, next) => {
     const validationErrors = validationResult(req);
 
-    if (!validationErrors.isEmpty()) {
-        const errors = validationErrors
-            .array()
-            .map((error) => `${error.msg}`);
-
-        const err = Error('Bad request.');
-        err.errors = errors;
-        err.status = 400;
-        err.title = 'Bad request.';
-        next(err);
-    }
-    next();
-
     // if (!validationErrors.isEmpty()) {
     //     const errors = validationErrors
     //         .array()
     //         .map((error) => `${error.msg}`);
 
-    //     // const errorsObj = {};
-    //     // validationErrors.array().forEach(error => {
-    //     //     errorsObj[error.title] = error.msg;
-    //     // });
-
-    //     const err = Error('Validation error');
+    //     const err = Error('Bad request.');
     //     err.errors = errors;
     //     err.status = 400;
-    //     err.title = 'Validation error.';
+    //     err.title = 'Bad request.';
     //     next(err);
     // }
     // next();
+
+    if (!validationErrors.isEmpty()) {
+        const errors = validationErrors
+            .array()
+            .map((error) => `${error.msg}`);
+
+        // const errorsObj = {};
+        // validationErrors.array().forEach(error => {
+        //     errorsObj[error.title] = error.msg;
+        // });
+
+        const err = Error('Validation error');
+        err.errors = errors;
+        err.status = 400;
+        err.title = 'Validation error.';
+        next(err);
+    }
+    next();
 };
 
 module.exports = {
