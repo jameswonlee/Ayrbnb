4f64a76afaa7c13db7c7a07b3c42160e305fe379
James Lee <jamesleex86@gmail.com>
2022-10-20 10:54:14 -0700

    Added footer with links to linkedIn and github

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import * as sessionActions from "./store/session";
 import Navigation from "./components/Navigation";
 import DisplayAllSpots from "./components/AllSpots";
 import SingleSpot from "./components/SingleSpot";
+import './index.css';
 
 function App() {
   const dispatch = useDispatch();
@@ -19,24 +20,37 @@ function App() {
       <Navigation isLoaded={isLoaded} />
       {/* <div className="center">
         <main> */}
-          {isLoaded && (
-            <Switch>
-              {/* <Route path='/signup'>
+      {isLoaded && (
+        <Switch>
+          {/* <Route path='/signup'>
                 <SignupFormPage />
               </Route> */}
-              <Route path='/spots/:spotId'>
-                <SingleSpot />
-              </Route>
-              <Route exact path='/'>
-                <DisplayAllSpots />
-              </Route>
-              <Route>
-                Page Not Found!
-              </Route>
-            </Switch>
-          )}
-        {/* </main>
+          <Route path='/spots/:spotId'>
+            <SingleSpot />
+          </Route>
+          <Route exact path='/'>
+            <DisplayAllSpots />
+          </Route>
+          <Route>
+            Page Not Found!
+          </Route>
+        </Switch>
+      )}
+      {/* </main>
       </div> */}
+      <footer className="footer">
+        <div className="footer-about">
+          An Airbnb 'clone' by James Lee
+        </div>
+        <div className="footer-links">
+          <a id="github" className="links-github" href="https://github.com/jameswonlee">
+            <i className="fa-brands fa-github fa-xl"></i>
+          </a>
+          <a id="linkedin" className="links-linkedin" href="https://www.linkedin.com/in/jameswonlee/">
+            <i className="fa-brands fa-linkedin fa-xl"></i>
+          </a>
+        </div>
+      </footer>
     </>
   );
 }
