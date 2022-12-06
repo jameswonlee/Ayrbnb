54ee1dc0eafbcefae64b2895fef5b734d391f5db
James Lee <jamesleex86@gmail.com>
2022-12-05 17:33:12 -0800

    Made github and linkedin links open in new tab

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -48,10 +48,10 @@ function App() {
           An Airbnb 'clone' by James Lee
         </div>
         <div className="footer-links">
-          <a id="github" className="links-github" href="https://github.com/jameswonlee">
+          <a id="github" className="links-github" href="https://github.com/jameswonlee" target="_blank">
             <i className="fa-brands fa-github fa-xl"></i>
           </a>
-          <a id="linkedin" className="links-linkedin" href="https://www.linkedin.com/in/jameswonlee/">
+          <a id="linkedin" className="links-linkedin" href="https://www.linkedin.com/in/jameswonlee/" target="_blank">
             <i className="fa-brands fa-linkedin fa-xl"></i>
           </a>
         </div>
